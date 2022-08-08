5d5ae0105b12f05d901a97918433adb07791295b
Tatiana <tatiana-bondareva@inbox.ru>
2022-08-09 00:11:41 +0300

    add Example 010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 9, 2, 4, 3, 5, 6, 7, 8, 9};
+
+int n = array.Length;
+int find = 9;
+
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find) 
+        {
+            Console.WriteLine(index);
+            break;
+        }
+    index++;
+}
